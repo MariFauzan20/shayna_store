1dc5b8791d688194f8531ec075e70b336302a446
Mar'i Fauzan <marifauzan20@gmail.com>
2021-01-02 10:32:27 +0700

    Add RelatedShayna.vue, ShoppingCart.vue, Success.vue and update WomanShayna.vue index.html, index.js, Product.vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Product from "../views/Product.vue";
+import ShoppingCart from "../views/ShoppingCart.vue";
+import Success from "../views/Success.vue";
 
 Vue.use(VueRouter);
 
@@ -22,8 +24,18 @@ const routes = [
   },
   {
     path: "/product",
-    name: "Product",
+    name: "product",
     component: Product,
+  },
+  {
+    path: "/cart",
+    name: "card",
+    component: ShoppingCart,
+  },
+  {
+    path: "/success",
+    name: "success",
+    component: Success,
   },
 ];
 
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,12 +2,12 @@
   <div class="Product">
     <HeaderShayna />
     <!-- Breadcrumb Section Begin -->
-    <div class="breacrumb-section">
+    <div class="breacrumb-section text-left">
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
             <div class="breadcrumb-text product-more">
-              <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+              <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
               <span>Detail</span>
             </div>
           </div>
@@ -24,36 +24,50 @@
             <div class="row">
               <div class="col-lg-6">
                 <div class="product-pic-zoom">
-                  <img class="product-big-img" src="img/mickey1.jpg" alt="" />
+                  <img class="product-big-img" :src="default_photo" alt="" />
                 </div>
                 <div class="product-thumbs">
                   <carousel
                     class="product-thumbs-track ps-slider"
                     :dots="false"
                     :nav="false"
-                    :autoplay="true"
-                    :loop="true"
                   >
-                    <div class="pt active" data-imgbigurl="img/mickey1.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[0])"
+                      :class="thumbs[0] == default_photo ? 'active' : ''"
+                    >
                       <img src="img/mickey1.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey2.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[1])"
+                      :class="thumbs[1] == default_photo ? 'active' : ''"
+                    >
                       <img src="img/mickey2.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey3.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[2])"
+                      :class="thumbs[2] == default_photo ? 'active' : ''"
+                    >
                       <img src="img/mickey3.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey4.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[3])"
+                      :class="thumbs[3] == default_photo ? 'active' : ''"
+                    >
                       <img src="img/mickey4.jpg" alt="" />
                     </div>
                   </carousel>
                 </div>
               </div>
               <div class="col-lg-6">
-                <div class="product-details">
+                <div class="product-details text-left">
                   <div class="pd-title">
                     <span>oranges</span>
                     <h3>Pure Pineapple</h3>
@@ -84,8 +98,8 @@
                     <h4>$495.00</h4>
                   </div>
                   <div class="quantity">
-                    <a href="shopping-cart.html" class="primary-btn pd-cart"
-                      >Add To Cart</a
+                    <router-link to="/cart" class="primary-btn pd-cart"
+                      >Add To Cart</router-link
                     >
                   </div>
                 </div>
@@ -96,6 +110,8 @@
       </div>
     </section>
     <!-- Product Shop Section End -->
+
+    <RelatedShayna />
     <FooterShayna />
   </div>
 </template>
@@ -105,6 +121,7 @@
 // import HelloWorld from "@/components/HelloWorld.vue";
 import HeaderShayna from "../components/HeaderShayna.vue";
 import FooterShayna from "../components/FooterShayna.vue";
+import RelatedShayna from "../components/RelatedShayna.vue";
 
 import carousel from "vue-owl-carousel";
 
@@ -114,6 +131,23 @@ export default {
     HeaderShayna,
     FooterShayna,
     carousel,
+    RelatedShayna,
+  },
+  data() {
+    return {
+      default_photo: "img/mickey1.jpg",
+      thumbs: [
+        "img/mickey1.jpg",
+        "img/mickey2.jpg",
+        "img/mickey3.jpg",
+        "img/mickey4.jpg",
+      ],
+    };
+  },
+  methods: {
+    changeImage(urlImage) {
+      this.default_photo = urlImage;
+    },
   },
 };
 </script>
